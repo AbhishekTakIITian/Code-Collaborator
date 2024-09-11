7a8b80f6693cc61c83ca8bad3e8680ca04e77d0e
Abhishek Anil Tak <101012978+AbhishekTakIITian@users.noreply.github.com>
2024-09-11 23:41:59 +0530

    First Commit
    Code is yet to be completed

---
+++ src/Components/Editor.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useRef } from 'react'
+import Codemirror from 'codemirror'
+import 'codemirror/lib/codemirror.css' 
+import 'codemirror/theme/dracula.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
+import ACTIONS from '../Actions'
+
+const Editor = ({socketRef}) => {
+  const editorRef = useRef(null);
+  useEffect(()=>{
+    async function init() {
+        editorRef.current = Codemirror.fromTextArea(document.getElementById('realtimeEditor'),{
+        mode:{ name:`javascript`, json:true},
+        theme: 'dracula', 
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
+
+      editorRef.current.on('change',(instance,changes)=>{
+          console.log('changes',changes);
+          const {origin} = changes;
+          const code = instance.getValue();
+          if(origin!=='setValue'){
+            socketRef.current.emit(ACTIONS.CODE_CHANGE);
+          }
+          console.log(code);
+      });
+
+      editorRef.current.setValue(`console.log('hello')`);
+    }
+    init();
+  },[]);
+  return (
+    <textarea id='realtimeEditor'></textarea>
+  )
+}
+
+export default Editor
---
+++ src/pages/Home.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom';
+const Home = () => {
+    const navigate = useNavigate();
+
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidV4();
+        setRoomId(id);
+        toast.success('Created a new room');
+    };
+    const joinRoom = () => {
+        if (!roomId || !username) {
+            toast.error('ROOM ID & username is required');
+            return;
+        }
+
+        // Redirect
+        navigate(`/editor/${roomId}`, {
+            state: {
+                username,
+            },
+        });
+    };
+    const handleInputEnter = (e)=>{
+        if(e.code === 'Enter'){
+            joinRoom();
+        }
+    }
+  return (
+    <div className='homePageWrapper'>
+        <div className='formWrapper'>
+            <img src='/code-sync.png' className='homeLogo'></img>
+            <h4 className='mainLabel'>Paste invitation room id</h4>
+            <div className='inputGroup'>
+                <input 
+                className='inputBox' 
+                type='text' 
+                placeholder='Room ID' 
+                value={roomId}
+                onChange={(e)=>setRoomId(e.target.value)}
+                onKeyUp={handleInputEnter}
+                ></input>
+                <input 
+                className='inputBox' 
+                type='text' 
+                placeholder='User Name'
+                value={username}
+                onChange={(e)=>setUsername(e.target.value)}
+                onKeyUp={handleInputEnter}
+                ></input>
+                <button className='btn joinBtn' onClick={(joinRoom)}>Join</button>
+                <span className='createInfo'>
+                    If you don't have an invite then  cereate &nbsp; 
+                    <a href='' onClick={createNewRoom} className='createNewBtn'>New room</a>
+                </span>
+            </div>
+        </div>
+        <h4 className='footer'>Build with ❤️ by &nbsp;<a href='https://github.com/AbhishekTakIITian'>Abhishek Tak</a></h4>
+    </div>
+  )
+}
+
+export default Home
